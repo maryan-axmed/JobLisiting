029d63fdebbeda5c0dde7ae37e473b67877adea1
maryan <12maxmed@gmail.com>
2024-02-15 13:48:59 +0000

    added dependencies

--- src/main/java/com/example/jobListing/JobListingApplication.java
+++ src/main/java/com/example/jobListing/JobListingApplication.java
@@ -3,11 +3,29 @@ package com.example.jobListing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+//@EnableSwagger2
 public class JobListingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobListingApplication.class, args);
 	}
 
+//	@Bean
+//	public Docket api() {
+//		return new Docket(DocumentationType.SWAGGER_2).select()
+//				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+//				.build().apiInfo(apiInfo()).useDefaultResponseMessages(false);
+//	}
+//
+//	@Bean
+//	public ApiInfo apiInfo() {
+//		final ApiInfoBuilder builder = new ApiInfoBuilder();
+//		return builder.build();
+//	}
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(JoblistingApplication.class, args);
+//	}
 }
