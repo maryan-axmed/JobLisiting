2b32930d2a9f11e2547984f4233a854f859588ff
maryan <12maxmed@gmail.com>
2024-02-21 13:09:58 +0000

    finished findbytext request/method

--- src/main/java/com/example/jobListing/Repository/SearchRepoImplementation.java
+++ src/main/java/com/example/jobListing/Repository/SearchRepoImplementation.java
@@ -1,18 +1,46 @@
 package com.example.jobListing.Repository;
 
 import com.example.jobListing.Models.Post;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
 import org.springframework.stereotype.Component;
 
+import org.bson.Document;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+
 
 @Component
 public class SearchRepoImplementation implements SearchRepo {
+    @Autowired
+    MongoClient client;
 
+    @Autowired
+    MongoConverter converter;
     @Override
     public List<Post> findByText(String text){
 
         final List<Post> post = new ArrayList<>();
+
+        MongoDatabase database = client.getDatabase("joblisitng");
+        MongoCollection<Document> collection = database.getCollection("jobPosts");
+        AggregateIterable<Document> result = collection.aggregate(Arrays.asList(new Document("$search",
+                        new Document("text",
+                                new Document("query", "java")
+                                        .append("path", Arrays.asList("techs", "desc")))),
+                new Document("$sort",
+                        new Document("exp", 1L)),
+                new Document("$limit", 5L)));
+
+
+        result.forEach(doc -> post.add(converter.read(Post.class,doc)));
+
         return post;
     }
 }
